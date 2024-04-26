bd111d9e30abe9ef7e97d9ed1b49d04336005569
danilovalerio <danilo_vs@hotmail.com>
2024-04-26 18:05:37 -0300

    Injection WeatherService

--- src/app/modules/wheater/page/wheater-home/wheater-home.component.ts
+++ src/app/modules/wheater/page/wheater-home/wheater-home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
 
 @Component({
   selector: 'app-wheater-home',
   templateUrl: './wheater-home.component.html',
-  styleUrl: './wheater-home.component.scss'
+  styleUrl: './wheater-home.component.scss',
 })
 export class WheaterHomeComponent {
-
+  constructor(private weatherService: WeatherService) {}
 }
