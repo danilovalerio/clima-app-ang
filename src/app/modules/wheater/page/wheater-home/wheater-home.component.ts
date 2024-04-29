d52d66a59530ab88d554c61902a049d06ad27ef3
danilovalerio <danilo_vs@hotmail.com>
2024-04-29 15:32:28 -0300

    Se desinscrever do Observable!

--- src/app/modules/wheater/page/wheater-home/wheater-home.component.ts
+++ src/app/modules/wheater/page/wheater-home/wheater-home.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WeatherService } from '../../services/weather.service';
 import { response } from 'express';
 import { error } from 'console';
 import { IWeatherDatas } from '../../../../models/interfaces/weather-datas.interface';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-wheater-home',
   templateUrl: './wheater-home.component.html',
   styleUrl: './wheater-home.component.scss',
 })
-export class WheaterHomeComponent implements OnInit {
+export class WheaterHomeComponent implements OnInit, OnDestroy {
+  //Quando referenciar observable usar $ no final (destroy$)
+  private readonly destroy$: Subject<void> = new Subject();
   initialCityName = 'São Paulo';
   weatherDatas!: IWeatherDatas;
 
@@ -20,13 +23,22 @@ export class WheaterHomeComponent implements OnInit {
   }
 
   getWeatherDatas(cityName: string): void {
-    this.weatherService.getWeatherDatas(cityName).subscribe({
-      next: (response) => {
-        console.log(response);
-        response && (this.weatherDatas = response);
-        console.log(this.weatherDatas.main);
-      },
-      error: (error) => console.log(error),
-    });
+    this.weatherService
+      .getWeatherDatas(cityName)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+          response && (this.weatherDatas = response);
+          console.log(this.weatherDatas.main);
+        },
+        error: (error) => console.log(error),
+      });
+  }
+
+  ngOnDestroy(): void {
+    // ao sair da tela dará um unsubscribe para não ficar escutando esse obsevable
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
